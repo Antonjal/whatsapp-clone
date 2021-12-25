5b4e5ff60fa976e0a0fd4767222e7efb97fa62ac
Anton Jalalipour <anton.jalalipour@gmail.com>
2021-12-25 00:18:29 -0500

    Update axios.js

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://api.render.com/deploy/srv-c7346l9t9ds6ri52anvg?key=Uh9qXjBTgMY'
+    baseURL: 'https://whatsapp-mern-clone.onrender.com'
 })
 
 export default instance;
