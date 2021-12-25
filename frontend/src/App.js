a0490e448888b1faf402e32b904aab670e0a1153
Anton Jalalipour <anton.jalalipour@gmail.com>
2021-12-24 19:42:03 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,41 @@
-import React from "react";
 import './App.css';
 import Chat from "./Chat";
 import Sidebar from './Sidebar.js';
- 
-
-
+import Pusher from 'pusher-js';
+import axios from "./axios";
 function App() {
+
+  const [messages, setMessages] =  useState([])
+
+  useEffect(() => {
+      axios.get('https://whatsapp-mern-clone.onrender.com/messages/sync')
+        .then(response => {
+          setMessages(response.data);
+        })
+    }, []);
+import React, {useEffect, useState} from "react";
+
+  useEffect(() => {
+    const pusher = new Pusher('0a3c26149df588744c42', {
+      cluster: 'us2'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (newMessage) => {
+      setMessages([...messages, newMessage]);
+    });
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    }
+  }, [messages])
+  console.log(messages);
+
   return (
     <div className="app">
       <div className="app__body">
       <Sidebar />
-      <Chat />
+      <Chat messages={messages}/>
       </div>
     </div>
   );
