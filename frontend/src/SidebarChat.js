60b4657bb63c6da90681bbaecd27e4747527b0e8
Anton Jalalipour <anton.jalalipour@gmail.com>
2022-01-10 22:15:51 -0500

    Update SidebarChat.js

--- frontend/src/SidebarChat.js
+++ frontend/src/SidebarChat.js
@@ -1,17 +1,27 @@
 import { Avatar } from '@mui/material'
-import React from 'react'
+import React, {useEffect,useState} from 'react'
 import './SidebarChat.css'
 
 function SidebarChat() {
-    return (
+
+    const [seed, setSeed] =  useState("")
+
+    useEffect(() => {
+        setSeed(Math.floor(Math.random() * 5000));
+        }, []);
+
+   
+
+    return(
         <div className="sidebarChat">
-            <Avatar />
+            <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
             <div className="sidebarChat__info">
                 <h2>Room's name</h2>
                 <p>This is the last message</p>
             </div>
         </div>
-    )
+    );
 }
 
 export default SidebarChat
+
