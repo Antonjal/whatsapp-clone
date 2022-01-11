867355681bf8fb2c2120e23097182b9ca2402d4f
Anton Jalalipour <anton.jalalipour@gmail.com>
2022-01-10 22:07:02 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,21 +4,22 @@ import mongoose from 'mongoose'
 import Messages from './dbMessages.js'
 import Pusher from 'pusher'
 import cors from 'cors'
+import {} from 'dotenv/config'
 //app config
 const app = express()
 const port = process.env.PORT || 9000
 const pusher = new Pusher({
-    appId: "1321278",
-    key: "0a3c26149df588744c42",
-    secret: "8b3501689e34cb9fea11",
-    cluster: "us2",
-    useTLS: true
+    appId: process.env.PUSHER_APP_ID,
+    key: process.env.PUSHER_KEY,
+    secret: process.env.PUSHER_SECRET,
+    cluster: process.env.PUSHER_CLUSTER,
+    useTLS: process.env.PUSHER_USE_TLS
   });
 //middleware
 app.use(express.json());
 app.use(cors());
 //DB config
-const connection_url ='mongodb+srv://anton:etKbezRL9WMM6a9@cluster0.uqldt.mongodb.net/whatsappdb?retryWrites=true&w=majority'
+const connection_url =`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.uqldt.mongodb.net/whatsappdb?retryWrites=true&w=majority`
 mongoose.connect(connection_url)
 
 const db = mongoose.connection;
