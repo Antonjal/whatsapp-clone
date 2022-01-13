bcac9873ce5ca6c472e9552ba3b7cc4b6955e02a
Anton Jalalipour <anton.jalalipour@gmail.com>
2022-01-13 16:51:22 -0500

    Update Sidebar.js

--- frontend/src/Sidebar.js
+++ frontend/src/Sidebar.js
@@ -11,9 +11,11 @@ function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar__header">
+                {/* This materialUI component imports my github profile pic as the avatar on the top left of Sidebar component */}
                 <Avatar src="https://avatars.githubusercontent.com/u/29285093?s=400&u=225cccc48de556f580fe34a3b1257612d1fb43de&v=4" />
                 <div className="sidebar__headerRight">
-                   <IconButton> {/* This materialUI component turns MaterialUI icons into clickable buttons */}
+                   {/* This materialUI component turns MaterialUI icons into clickable buttons */}
+                   <IconButton> 
                         <DonutLargeIcon/>
                    </IconButton>
                    <IconButton>
